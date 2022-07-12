27e9ba2d8e142a07a2be3e2139712c40be0f0280
jihyune <rlawlcns96@gmail.com>
2022-07-12 19:54:15 +0900

    Initial commit

---
+++ src/main/java/jihyune/blog/JihyuneblogApplication.java
@@ -0,0 +1,13 @@
+package jihyune.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JihyuneblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JihyuneblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jihyune/blog/JihyuneblogApplicationTests.java
@@ -0,0 +1,13 @@
+package jihyune.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JihyuneblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
